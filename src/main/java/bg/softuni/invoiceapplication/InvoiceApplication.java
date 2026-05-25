93d3370e4c734e6f188ff01906f72631485b5198
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-05-25 17:12:56 +0300

    Initialize Spring Boot project

---
+++ src/main/java/bg/softuni/invoiceapplication/InvoiceApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.invoiceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceApplication.class, args);
+    }
+
+}
