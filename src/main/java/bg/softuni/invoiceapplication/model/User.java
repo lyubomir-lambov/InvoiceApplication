1750713ffc9a7eaac49f48ac1cbc6657fb98f475
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-05-25 20:00:43 +0300

    Edited: UserRegistrationRequestDTO.class

--- src/main/java/bg/softuni/invoiceapplication/model/User.java
+++ src/main/java/bg/softuni/invoiceapplication/model/User.java
@@ -2,10 +2,7 @@ package bg.softuni.invoiceapplication.model;
 
 import bg.softuni.invoiceapplication.model.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User extends BaseEntity {
 
     @Column(nullable = false, unique = true)
--- src/main/java/bg/softuni/invoiceapplication/model/dto/UserRegistrationRequestDTO.java
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/UserRegistrationRequestDTO.java
@@ -1,9 +1,28 @@
 package bg.softuni.invoiceapplication.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegistrationRequestDTO {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
+
+    @NotBlank(message = "Password confirmation is required")
+    @Size(min = 6, max = 20, message = "Password confirmation must be between 6 and 20 characters")
     private String passwordConfirm;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
     private String email;
 }
