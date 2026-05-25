f0f8fab23e8a4669435ee68bf1144309b7225184
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-05-25 20:23:45 +0300

    Created: PasswordEncoder.class BeanConfiguration.class

--- src/main/java/bg/softuni/invoiceapplication/service/impl/UserServiceImpl.java
+++ src/main/java/bg/softuni/invoiceapplication/service/impl/UserServiceImpl.java
@@ -2,14 +2,17 @@ package bg.softuni.invoiceapplication.service.impl;
 
 import bg.softuni.invoiceapplication.repository.UserRepository;
 import bg.softuni.invoiceapplication.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 }
