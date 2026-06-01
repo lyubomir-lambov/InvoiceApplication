42fea9f847487ffe6f211f9e97dbd628776ade38
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-01 18:19:20 +0300

    Edited: UserController.java registerUser registerSuccess

--- src/main/java/bg/softuni/invoiceapplication/web/UserController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/UserController.java
@@ -1,18 +1,46 @@
 package bg.softuni.invoiceapplication.web;
 
 import bg.softuni.invoiceapplication.model.dto.UserRegistrationRequestDTO;
+import bg.softuni.invoiceapplication.model.dto.UserRegistrationResponseDTO;
+import bg.softuni.invoiceapplication.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/register")
-    public String registerUser(Model model) {
+    public String registerForm(Model model) {
         model.addAttribute("user", new UserRegistrationRequestDTO());
         return "user-register";
     }
+
+    @PostMapping("/register")//! Binding result
+    public String registerUser(@Valid UserRegistrationRequestDTO userRegistrationRequestDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        UserRegistrationResponseDTO userRegistrationResponseDTO = userService.registerUser(userRegistrationRequestDTO);
+        redirectAttributes.addFlashAttribute("user", userRegistrationResponseDTO);
+        return "redirect:/users/register/success";
+    }
+
+    @GetMapping("/register/success")
+    public String registerSuccess() {
+        return "user-register-success";
+    }
+
+
 }
