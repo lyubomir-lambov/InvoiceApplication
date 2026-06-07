a883c6d77d5ca4a05895ae3af3048fb8d89224b2
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 15:29:47 +0300

    Created: ClientCreateRequestDTO.class

---
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/ClientCreateRequestDTO.java
@@ -0,0 +1,39 @@
+package bg.softuni.invoiceapplication.model.dto;
+
+import bg.softuni.invoiceapplication.model.enums.Country;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClientCreateRequestDTO {
+
+    @NotBlank(message = "Client name required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String displayName;
+
+    @NotBlank(message = "Company name required")
+    @Size(min = 2, max = 255, message = "Company name must be between 2 and 255 characters")
+    private String companyName;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String phoneNumber;
+
+    @NotNull(message = "Country required")
+    private Country country;
+
+    private String address;
+
+    @NotNull(message = "VAT registered selection required")
+    private Boolean vatRegistered;
+
+    @Size(max = 32, message = "VAT number must be up to 32 characters")
+    private String vatNumber;
+
+}
