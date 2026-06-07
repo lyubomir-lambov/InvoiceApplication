b242c124ba4b1ec4e7243b8d267cd7aa1eb671f9
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 17:14:24 +0300

    Edited: ClientRepository.java

--- src/main/java/bg/softuni/invoiceapplication/repository/ClientRepository.java
+++ src/main/java/bg/softuni/invoiceapplication/repository/ClientRepository.java
@@ -4,11 +4,11 @@ import bg.softuni.invoiceapplication.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
-    boolean existByDisplayName(String displayName);
-    boolean existByVatNumber(String vatNumber);
+    boolean existsByDisplayName(String displayName);
+
+    boolean existsByVatNumber(String vatNumber);
 }
