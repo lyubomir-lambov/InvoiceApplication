0b447c4dada6ef05e505edac41612db4e579f76b
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 17:48:02 +0300

    Created: ClientShowAllDTO.java

---
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/ClientShowAllDTO.java
@@ -0,0 +1,33 @@
+package bg.softuni.invoiceapplication.model.dto;
+
+import bg.softuni.invoiceapplication.model.enums.Country;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ClientShowAllDTO {
+
+    private UUID id;
+
+    private String displayName;
+
+    private String companyName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private Country country;
+
+    private String address;
+
+    private boolean vatRegistered;
+
+    private String vatNumber;
+
+    private boolean active;
+
+}
