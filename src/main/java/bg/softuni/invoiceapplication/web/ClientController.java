edb711cbbc6b8a7ea275c2e41c0c6d2146975f7b
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 16:13:07 +0300

    Edited: ClientController.java client-create.html Динамично се взема Country от Enum

--- src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
@@ -1,6 +1,7 @@
 package bg.softuni.invoiceapplication.web;
 
 import bg.softuni.invoiceapplication.model.dto.ClientCreateRequestDTO;
+import bg.softuni.invoiceapplication.model.enums.Country;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class ClientController {
     @GetMapping("create")
     public String createClient(Model model) {
         model.addAttribute("client", new ClientCreateRequestDTO());
+        model.addAttribute("countries", Country.values());
         return "client-create";
     }
 }
