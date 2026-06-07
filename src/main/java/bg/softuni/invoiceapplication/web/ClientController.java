4ce485411219cf5d171b0a68f93cbea7bc63c954
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 15:40:52 +0300

    Edited: ClientController.java

--- src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
@@ -1,13 +1,23 @@
 package bg.softuni.invoiceapplication.web;
 
+import bg.softuni.invoiceapplication.model.dto.ClientCreateRequestDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/clients")
 public class ClientController {
 
-    @GetMapping("/clients")
+    @GetMapping("")
     public String clients() {
         return "clients";
+    }
+
+    @GetMapping("create")
+    public String createClient(Model model) {
+        model.addAttribute("client", new ClientCreateRequestDTO());
+        return "client-create";
     }
 }
