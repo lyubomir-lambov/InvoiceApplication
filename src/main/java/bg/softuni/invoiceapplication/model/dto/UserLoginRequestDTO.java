69730c91415ca7beca7d7fbe8f0d29eaae298987
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 10:15:06 +0300

    Created: UserLoginRequestDTO.java

---
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/UserLoginRequestDTO.java
@@ -0,0 +1,20 @@
+package bg.softuni.invoiceapplication.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequestDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    private String password;
+
+}
