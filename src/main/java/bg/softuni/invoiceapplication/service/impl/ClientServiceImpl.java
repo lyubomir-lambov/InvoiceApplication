f0be3e9d1f354e36f974556e4098d21400f1e3be
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 16:37:08 +0300

    Edited: ClientController.java

--- src/main/java/bg/softuni/invoiceapplication/service/impl/ClientServiceImpl.java
+++ src/main/java/bg/softuni/invoiceapplication/service/impl/ClientServiceImpl.java
@@ -13,4 +13,5 @@ public class ClientServiceImpl implements ClientService {
     public ClientServiceImpl(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
+
 }
--- src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/ClientController.java
@@ -2,9 +2,13 @@ package bg.softuni.invoiceapplication.web;
 
 import bg.softuni.invoiceapplication.model.dto.ClientCreateRequestDTO;
 import bg.softuni.invoiceapplication.model.enums.Country;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,10 +20,23 @@ public class ClientController {
         return "clients";
     }
 
-    @GetMapping("create")
-    public String createClient(Model model) {
+    @GetMapping("/create")
+    public String showCreateClientForm(Model model) {
         model.addAttribute("client", new ClientCreateRequestDTO());
         model.addAttribute("countries", Country.values());
         return "client-create";
+    }
+
+    @PostMapping("/create")
+    public String createClient(@Valid @ModelAttribute("client")
+                               ClientCreateRequestDTO clientCreateRequestDTO,
+                               BindingResult bindingResult,
+                               Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("countries", Country.values());
+            return "client-create";
+        }
+        //! сървис регистрация
+        return "redirect:/clients";
     }
 }
