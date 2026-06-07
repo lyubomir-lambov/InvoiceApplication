2d36b9526c112b6644b05d3dd65e22bf26e8b64f
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 14:49:21 +0300

    Created: ClientRepository.java

---
+++ src/main/java/bg/softuni/invoiceapplication/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.invoiceapplication.repository;
+
+import bg.softuni.invoiceapplication.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+}
