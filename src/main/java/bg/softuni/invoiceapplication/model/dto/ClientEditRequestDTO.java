2ddb9a9ff7ac25201e06476caab3fa8448221cf1
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 19:37:58 +0300

    Edited: ClientEditRequestDTO.java

--- src/main/java/bg/softuni/invoiceapplication/model/dto/ClientEditRequestDTO.java
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/ClientEditRequestDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 public class ClientEditRequestDTO {
 
     @NotNull(message = "Client id required")
