2a0d6179a6a09163e7759f1f21676cd9c5ba0206
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-07 17:14:35 +0300

    Edited: ClientService.java

--- src/main/java/bg/softuni/invoiceapplication/service/ClientService.java
+++ src/main/java/bg/softuni/invoiceapplication/service/ClientService.java
@@ -1,4 +1,8 @@
 package bg.softuni.invoiceapplication.service;
 
+import bg.softuni.invoiceapplication.model.Client;
+import bg.softuni.invoiceapplication.model.dto.ClientCreateRequestDTO;
+
 public interface ClientService {
+    Client createClient(ClientCreateRequestDTO clientCreateRequestDTO);
 }
--- src/main/java/bg/softuni/invoiceapplication/service/impl/ClientServiceImpl.java
+++ src/main/java/bg/softuni/invoiceapplication/service/impl/ClientServiceImpl.java
@@ -1,5 +1,8 @@
 package bg.softuni.invoiceapplication.service.impl;
 
+import bg.softuni.invoiceapplication.mapper.client.ClientMapper;
+import bg.softuni.invoiceapplication.model.Client;
+import bg.softuni.invoiceapplication.model.dto.ClientCreateRequestDTO;
 import bg.softuni.invoiceapplication.repository.ClientRepository;
 import bg.softuni.invoiceapplication.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +11,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClientServiceImpl implements ClientService {
     private final ClientRepository clientRepository;
+    private final ClientMapper clientMapper;
 
     @Autowired
-    public ClientServiceImpl(ClientRepository clientRepository) {
+    public ClientServiceImpl(ClientRepository clientRepository, ClientMapper clientMapper) {
         this.clientRepository = clientRepository;
+        this.clientMapper = clientMapper;
+    }
+
+    @Override
+    public Client createClient(ClientCreateRequestDTO clientCreateRequestDTO) {
+        if (clientRepository.existsByDisplayName(clientCreateRequestDTO.getDisplayName())) {
+            throw new RuntimeException("Client with display name " + clientCreateRequestDTO.getDisplayName() + " already exists");
+            //! След време да сменя грешката
+        }
+        String vatNumber = clientCreateRequestDTO.getVatNumber();
+        if (vatNumber != null && !vatNumber.isBlank()
+                && clientRepository.existsByVatNumber(vatNumber)) {
+            throw new RuntimeException("Client with vatNumber " + vatNumber + " already exists");
+            //! След време да сменя грешката
+        }
+
+        Client clientToSave = clientMapper.fromClientCreateRequestDTOToClient(clientCreateRequestDTO);
+        Client savedClient = clientRepository.save(clientToSave);
+        return savedClient;
+
     }
 
 }
