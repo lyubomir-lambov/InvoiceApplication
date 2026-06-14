798879bf9d95d5a283050bb2ae7df9a41e1ca44c
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-14 14:01:42 +0300

    Edited: UserSessionDTO.java Getters Setters

--- src/main/java/bg/softuni/invoiceapplication/model/dto/UserSessionDTO.java
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/UserSessionDTO.java
@@ -1,9 +1,13 @@
 package bg.softuni.invoiceapplication.model.dto;
 
 import bg.softuni.invoiceapplication.model.enums.UserRole;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 public class UserSessionDTO {
     private UUID id;
     private UserRole role;
