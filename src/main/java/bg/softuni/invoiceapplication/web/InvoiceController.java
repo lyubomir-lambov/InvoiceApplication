1132611a2f54954385a33fab01395e3e46ab68ad
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-14 18:43:51 +0300

    Added: Username visualize in /invoices

--- src/main/java/bg/softuni/invoiceapplication/web/InvoiceController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/InvoiceController.java
@@ -1,13 +1,28 @@
 package bg.softuni.invoiceapplication.web;
 
+import bg.softuni.invoiceapplication.security.SessionUser;
+import bg.softuni.invoiceapplication.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
 
 @Controller
 public class InvoiceController {
 
+    private final UserService userService;
+
+    public InvoiceController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/invoices")
-    public String invoices() {
+    public String invoices(HttpSession httpSession, Model model) {
+        UUID userId = SessionUser.getUserId(httpSession);
+        String username = userService.getUsernameById(userId);
+        model.addAttribute("username", username);
         return "invoices";
     }
 }
