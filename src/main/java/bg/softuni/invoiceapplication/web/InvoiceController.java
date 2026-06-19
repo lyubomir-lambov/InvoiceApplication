b6c22c17f222cd1ce0d933285de0a3368e5b3a3d
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-19 16:05:58 +0300

    Edited: InvoiceController.java @PostMapping("/create") public String createInvoice - Added private void addCreateInvoiceFormAttributes - Added

--- src/main/java/bg/softuni/invoiceapplication/web/InvoiceController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/InvoiceController.java
@@ -1,14 +1,19 @@
 package bg.softuni.invoiceapplication.web;
 
+import bg.softuni.invoiceapplication.model.dto.InvoiceCreateRequestDTO;
 import bg.softuni.invoiceapplication.model.enums.InvoiceType;
 import bg.softuni.invoiceapplication.security.SessionUser;
 import bg.softuni.invoiceapplication.service.ClientService;
 import bg.softuni.invoiceapplication.service.InvoiceService;
 import bg.softuni.invoiceapplication.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
@@ -46,5 +51,28 @@ public class InvoiceController {
         model.addAttribute("clients", clientService.findAllActiveClientsForSelect());
 
         return "invoice-create";
+    }
+
+    @PostMapping("/create")
+    public String createInvoice(@Valid @ModelAttribute("invoice") InvoiceCreateRequestDTO invoiceCreateRequestDTO,
+                                BindingResult bindingResult,
+                                HttpSession httpSession,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            addCreateInvoiceFormAttributes(httpSession, model);
+            return "invoice-create";
+        }
+
+        invoiceService.createInvoice(invoiceCreateRequestDTO);
+        return "redirect:/invoices";
+    }
+
+    private void addCreateInvoiceFormAttributes(HttpSession httpSession, Model model) {
+        UUID userId = SessionUser.getUserId(httpSession);
+        String username = userService.getUsernameById(userId);
+
+        model.addAttribute("username", username);
+        model.addAttribute("invoiceTypes", InvoiceType.values());
+        model.addAttribute("clients", clientService.findAllActiveClientsForSelect());
     }
 }
