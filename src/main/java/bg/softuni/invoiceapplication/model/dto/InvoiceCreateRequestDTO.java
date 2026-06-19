3710fdb8ef9726fb96373e812d078021a883baf7
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-19 12:05:13 +0300

    Edited: InvoiceCreateRequestDTO.java Подаваме датите във формат DateTimeFormat.ISO.DATE, защото браузърът очаква стойност във формат yyyy-MM-dd

--- src/main/java/bg/softuni/invoiceapplication/model/dto/InvoiceCreateRequestDTO.java
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/InvoiceCreateRequestDTO.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -29,9 +30,11 @@ public class InvoiceCreateRequestDTO {
     private String invoiceNumber;
 
     @NotNull(message = "Issue date required")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate issueDate;
 
     @NotNull(message = "Due date required")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dueDate;
 
     @NotNull(message = "Client required")
