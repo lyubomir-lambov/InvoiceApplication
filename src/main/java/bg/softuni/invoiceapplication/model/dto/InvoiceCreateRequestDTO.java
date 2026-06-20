f355453da9dea71b1e26e3a1568cf9011122377d
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-20 08:35:31 +0300

    Edited: InvoiceCreateRequestDTO.java Added: private List<InvoiceLineItemCreateRequestDTO> with validation

--- src/main/java/bg/softuni/invoiceapplication/model/dto/InvoiceCreateRequestDTO.java
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/InvoiceCreateRequestDTO.java
@@ -1,7 +1,9 @@
 package bg.softuni.invoiceapplication.model.dto;
 
 import bg.softuni.invoiceapplication.model.enums.InvoiceType;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -10,6 +12,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -39,4 +43,8 @@ public class InvoiceCreateRequestDTO {
 
     @NotNull(message = "Client required")
     private UUID clientId;
+
+    @Valid
+    @NotEmpty(message = "At least one invoice line item is required")
+    private List<InvoiceLineItemCreateRequestDTO> lineItems = new ArrayList<>();
 }
