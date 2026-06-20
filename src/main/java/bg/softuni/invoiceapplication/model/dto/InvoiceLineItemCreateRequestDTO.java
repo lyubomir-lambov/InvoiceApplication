5848158d673228e964d77296babe9d5f21014295
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-20 08:16:56 +0300

    Created: InvoiceLineItemCreateRequestDTO.java

---
+++ src/main/java/bg/softuni/invoiceapplication/model/dto/InvoiceLineItemCreateRequestDTO.java
@@ -0,0 +1,35 @@
+package bg.softuni.invoiceapplication.model.dto;
+
+import bg.softuni.invoiceapplication.model.enums.MeasurementUnit;
+import bg.softuni.invoiceapplication.model.enums.VatRate;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class InvoiceLineItemCreateRequestDTO {
+
+    @NotBlank(message = "Description required")
+    @Size(max = 255, message = "Description must be up to 255 characters")
+    private String description;
+
+    @NotNull(message = "Quantity required")
+    @DecimalMin(value = "0.01", message = "Quantity must be greater than zero")
+    private BigDecimal quantity;
+
+    @NotNull(message = "Measurement unit required")
+    private MeasurementUnit measurementUnit;
+
+    @NotNull(message = "Unit price required")
+    @DecimalMin(value = "0.00", message = "Unit price cannot be negative")
+    private BigDecimal unitPrice;
+
+    @NotNull(message = "VAT rate required")
+    private VatRate vatRate;
+}
