196182b295c5645d8e9d9772d4d86a4610a19e89
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-06-21 19:33:49 +0300

    Fixed: Invoice Application Logo if logged no need to load home page with register and login

--- src/main/java/bg/softuni/invoiceapplication/web/HomeController.java
+++ src/main/java/bg/softuni/invoiceapplication/web/HomeController.java
@@ -1,5 +1,7 @@
 package bg.softuni.invoiceapplication.web;
 
+import bg.softuni.invoiceapplication.security.SessionUser;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession httpSession) {
+        if (SessionUser.getUserId(httpSession) != null) {
+            return "redirect:/invoices";
+        }
+
         return "index";
     }
 }
