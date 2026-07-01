7c6da577ec923cc2a10641bf18991e396c991a82
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-07-01 20:33:10 +0300

    Edited: WebMvcConfiguration.java No need SessionInterceptor

--- src/main/java/bg/softuni/invoiceapplication/config/WebMvcConfiguration.java
+++ src/main/java/bg/softuni/invoiceapplication/config/WebMvcConfiguration.java
@@ -1,21 +1,9 @@
 package bg.softuni.invoiceapplication.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    private final SessionInterceptor sessionInterceptor;
 
-    public WebMvcConfiguration(SessionInterceptor sessionInterceptor) {
-        this.sessionInterceptor = sessionInterceptor;
-    }
-
-    @Override
-    public void  addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(sessionInterceptor)
-        .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/images/**");
-    }
 }
