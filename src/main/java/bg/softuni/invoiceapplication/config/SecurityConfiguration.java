6c1d60d2d0e2a67aece32c1b5c8f49256740076c
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-07-01 20:47:24 +0300

    Created SecurityFilterChain in SecurityConfiguration Renamed WebMvcConfiguration -> SecurityConfiguration

---
+++ src/main/java/bg/softuni/invoiceapplication/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package bg.softuni.invoiceapplication.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(matchers -> matchers
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .requestMatchers("/", "/login", "/users/register", "/users/register/success", "/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+
+        http.formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/invoices", true)
+                .failureUrl("/login?error=true")
+                .permitAll());
+
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/"));
+
+
+        return http.build();
+    }
+}
--- src/main/java/bg/softuni/invoiceapplication/config/WebMvcConfiguration.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.invoiceapplication.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
-
-}
