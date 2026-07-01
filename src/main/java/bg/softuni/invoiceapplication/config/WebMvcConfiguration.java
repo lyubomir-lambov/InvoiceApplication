bb5737b6dce5861963fb3655c7c86c00ecc91e26
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-07-01 20:29:35 +0300

    Deleted: SessionInterceptor.java

--- src/main/java/bg/softuni/invoiceapplication/config/WebMvcConfiguration.java
+++ src/main/java/bg/softuni/invoiceapplication/config/WebMvcConfiguration.java
@@ -1,6 +1,5 @@
 package bg.softuni.invoiceapplication.config;
 
-import bg.softuni.invoiceapplication.security.SessionInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
