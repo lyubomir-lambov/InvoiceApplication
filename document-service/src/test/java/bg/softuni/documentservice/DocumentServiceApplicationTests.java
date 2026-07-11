48532726ab00735b6e89e64a6559f45583e1e625
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-07-11 10:19:59 +0300

    Setup: Module document-service application-properties

---
+++ document-service/src/test/java/bg/softuni/documentservice/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.documentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
