82fd10f325e8acd0e25528e335fe44b823ad091c
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 21:21:34 +0300

    Edited: InvoiceHistoryService

--- invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/web/InvoiceHistoryController.java
+++ invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/web/InvoiceHistoryController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,5 +37,11 @@ public class InvoiceHistoryController {
     @GetMapping("/invoices/{invoiceId}")
     public ResponseEntity<List<InvoiceHistoryResponseDTO>> findHistoryByInvoiceId(@PathVariable UUID invoiceId) {
         return ResponseEntity.ok(invoiceHistoryService.findHistoryByInvoiceId(invoiceId));
+    }
+
+    @DeleteMapping("/invoices/{invoiceId}")
+    public ResponseEntity<Void> clearHistoryByInvoiceId(@PathVariable UUID invoiceId) {
+        invoiceHistoryService.clearHistoryByInvoiceId(invoiceId);
+        return ResponseEntity.noContent().build();
     }
 }
