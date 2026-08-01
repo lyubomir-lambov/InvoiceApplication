d3c5fc70a059f64bc07434354cb7bd560a9cd88b
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 12:35:08 +0300

    Created: InvoiceHistoryRecordRepository

---
+++ invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/repository/InvoiceHistoryRecordRepository.java
@@ -0,0 +1,17 @@
+package bg.softuni.invoicehistoryservice.repository;
+
+import bg.softuni.invoicehistoryservice.model.entity.InvoiceHistoryRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface InvoiceHistoryRecordRepository extends JpaRepository<InvoiceHistoryRecord, UUID> {
+
+    List<InvoiceHistoryRecord> findByInvoiceIdOrderByRevisionNumberDesc(UUID invoiceId);
+
+    Optional<InvoiceHistoryRecord> findTopByInvoiceIdOrderByRevisionNumberDesc(UUID invoiceId);
+}
