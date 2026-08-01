03ebc74ae85b321ae4b98f0e88d64bece6b58974
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 16:48:59 +0300

    Edited: InvoiceApplication EnableFeignClients

--- src/main/java/bg/softuni/invoiceapplication/InvoiceApplication.java
+++ src/main/java/bg/softuni/invoiceapplication/InvoiceApplication.java
@@ -2,8 +2,10 @@ package bg.softuni.invoiceapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InvoiceApplication {
 
     public static void main(String[] args) {
