86521e142c4f1a7994e50b1ed7cbaf0a64b7080d
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 17:33:51 +0300

    Created: InvoiceHistoryIntegrationService

---
+++ src/main/java/bg/softuni/invoiceapplication/service/InvoiceHistoryIntegrationService.java
@@ -0,0 +1,14 @@
+package bg.softuni.invoiceapplication.service;
+
+import bg.softuni.invoiceapplication.model.dto.invoicehistory.InvoiceHistoryCreateRequestDTO;
+import bg.softuni.invoiceapplication.model.dto.invoicehistory.InvoiceHistoryResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface InvoiceHistoryIntegrationService {
+
+    InvoiceHistoryResponseDTO createHistoryRecord(InvoiceHistoryCreateRequestDTO invoiceHistoryCreateRequestDTO);
+
+    List<InvoiceHistoryResponseDTO> findHistoryByInvoiceId(UUID invoiceId);
+}
