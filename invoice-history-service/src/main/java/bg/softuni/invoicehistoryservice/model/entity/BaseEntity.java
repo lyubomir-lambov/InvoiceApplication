f8e7117813748881af1eaf1de030d15e88716262
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 12:23:32 +0300

    Created: BaseEntity

---
+++ invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/model/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package bg.softuni.invoicehistoryservice.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
