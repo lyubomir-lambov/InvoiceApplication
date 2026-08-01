9e03f1f6633b9a970136a6d071be8583bb3caec5
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-01 14:53:41 +0300

    Created: InvoiceHistoryResponseDTO

---
+++ invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/model/dto/InvoiceHistoryResponseDTO.java
@@ -0,0 +1,57 @@
+package bg.softuni.invoicehistoryservice.model.dto;
+
+import bg.softuni.invoicehistoryservice.model.enums.InvoiceHistoryAction;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InvoiceHistoryResponseDTO {
+
+    private UUID id;
+
+    private UUID invoiceId;
+
+    private String invoiceNumber;
+
+    private String invoiceType;
+
+    private String invoiceStatus;
+
+    private Integer revisionNumber;
+
+    private InvoiceHistoryAction action;
+
+    private String currency;
+
+    private String clientDisplayName;
+
+    private String clientCompanyName;
+
+    private LocalDate issueDate;
+
+    private LocalDate dueDate;
+
+    private BigDecimal totalWithoutVat;
+
+    private BigDecimal totalVat;
+
+    private BigDecimal totalWithVat;
+
+    private String snapshotJson;
+
+    private String performedByUsername;
+
+    private LocalDateTime createdOn;
+}
