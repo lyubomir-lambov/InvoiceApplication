da0b29295cf10d7bd0c0f54196eafb42bac6d4a9
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-02 10:34:36 +0300

    Edited: InvoiceRepository

--- src/main/java/bg/softuni/invoiceapplication/repository/InvoiceRepository.java
+++ src/main/java/bg/softuni/invoiceapplication/repository/InvoiceRepository.java
@@ -1,9 +1,11 @@
 package bg.softuni.invoiceapplication.repository;
 
 import bg.softuni.invoiceapplication.model.entity.Invoice;
+import bg.softuni.invoiceapplication.model.enums.InvoiceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,6 +15,8 @@ public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
     Optional<Invoice> findTopByOrderByInvoiceSequenceDesc();
 
     List<Invoice> findByClientCompanyNameContainingIgnoreCaseOrderByInvoiceSequenceDesc(String clientCompanyName);
+
+    List<Invoice> findAllByStatusAndDueDateBefore(InvoiceStatus status, LocalDate dueDate);
 
     boolean existsByClientId(UUID clientId);
 }
