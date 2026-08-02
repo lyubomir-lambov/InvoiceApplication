11265fc202091ca5740445d69ca6b2bb9d3c739a
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-02 10:35:18 +0300

    Edited: InvoiceApplication

--- src/main/java/bg/softuni/invoiceapplication/InvoiceApplication.java
+++ src/main/java/bg/softuni/invoiceapplication/InvoiceApplication.java
@@ -3,9 +3,11 @@ package bg.softuni.invoiceapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class InvoiceApplication {
 
     public static void main(String[] args) {
