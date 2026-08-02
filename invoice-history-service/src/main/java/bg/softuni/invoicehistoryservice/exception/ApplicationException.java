7aac7b10d7af6ed2b43615aea555f9bb05f4889e
lyubomir-lambov <153599990+lyubomir-lambov@users.noreply.github.com>
2026-08-02 10:02:54 +0300

    Edited: ApplicationException

--- invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/exception/ApplicationException.java
+++ invoice-history-service/src/main/java/bg/softuni/invoicehistoryservice/exception/ApplicationException.java
@@ -1,16 +1,23 @@
 package bg.softuni.invoicehistoryservice.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApplicationException extends RuntimeException {
 
     private final String errorCode;
     private final String errorTitle;
+    private final HttpStatus httpStatus;
 
     public ApplicationException(String message, String errorCode, String errorTitle) {
+        this(message, errorCode, errorTitle, HttpStatus.BAD_REQUEST);
+    }
+
+    public ApplicationException(String message, String errorCode, String errorTitle, HttpStatus httpStatus) {
         super(message);
         this.errorCode = errorCode;
         this.errorTitle = errorTitle;
+        this.httpStatus = httpStatus;
     }
 }
